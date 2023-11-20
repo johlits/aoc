6cfe67336afca7645ad9e3b1b4aa9c760787178a
Johan Litsfeldt <johan.litsfeldt@uu.se>
2023-11-20 10:51:20 +0100

    add 2021 and template folder

---
+++ aocTemplate/day01/Day1.cs
@@ -0,0 +1,17 @@
+﻿public class Day1
+{
+    public static void Run()
+    {
+        using (StreamReader file = new StreamReader("day01/p.in"))
+        {
+            string? ln;
+            while ((ln = file.ReadLine()) != null)
+            {
+                Console.WriteLine(ln);
+            }
+
+            file.Close();
+        }
+    }
+}
+
