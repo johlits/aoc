eed0ce457722405cbd71e17e9a787bf98252512a
Johan Litsfeldt <johan.litsfeldt@uu.se>
2023-11-22 14:16:19 +0100

    Remove visualizer

--- visualizer/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace visualizer;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
