13423934f8f1b26f6db903f450a0b7d728f6b66b
Johan Litsfeldt <2831146+johlits@users.noreply.github.co>
2023-12-04 06:32:52 +0100

    prep day 5

---
+++ aoc2023/day05/Day5.cs
@@ -0,0 +1,18 @@
+﻿
+public class Day5
+{
+    public static void Run()
+    {
+        using (StreamReader file = new StreamReader("day05/p.in"))
+        {
+            string? ln;
+            while ((ln = file.ReadLine()) != null)
+            {
+                Console.WriteLine(ln);
+            }
+
+            file.Close();
+        }
+    }
+}
+
