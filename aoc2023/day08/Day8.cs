8e7d561579e46116a2378efc1068e89ae3b791e4
Johan Litsfeldt <2831146+johlits@users.noreply.github.co>
2023-12-07 08:05:40 +0100

    Add day 7 2023

---
+++ aoc2023/day08/Day8.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks.Sources;
+
+public class Day8
+{
+    public static void Run()
+    {
+        using (StreamReader file = new StreamReader("day08/p.in"))
+        {
+            string? ln;
+            while ((ln = file.ReadLine()) != null)
+            {
+                Console.WriteLine(ln);
+            }
+
+            file.Close();
+        }
+    }
+}
+
