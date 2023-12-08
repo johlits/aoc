502835cfb9216fef08a0af8182f2d736c5f64d66
Johan Litsfeldt <2831146+johlits@users.noreply.github.co>
2023-12-08 06:43:06 +0100

    Day 8 2023

---
+++ aoc2023/day09/Day9.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Threading.Tasks.Sources;
+
+public class Day9
+{
+    public static void Run()
+    {
+        using (StreamReader file = new StreamReader("day09/p.in"))
+        {
+            string? ln;
+            while ((ln = file.ReadLine()) != null)
+            {
+                Console.WriteLine(ln);
+            }
+
+            file.Close();
+        }
+    }
+}
+
