0642df24d226faf0ebe30ebe6e1450db1700eaa4
johlits <2831146+johlits@users.noreply.github.com>
2023-12-08 18:44:26 +0100

    Add 2019 day 1

---
+++ aoc2019/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+open Day1
+
+let run argv =
+    let result1 = Day.part1
+    let result2 = Day.part2
+    printfn "Part 1: %d" result1
+    printfn "Part 2: %d" result2
+
+[<EntryPoint>]
+let main argv =
+    run argv
+    0
---
+++ aoc2019/day/Day.fs
@@ -0,0 +1,11 @@
+﻿module Day
+open System
+
+let input = 
+    IO.File.ReadAllLines("day/p.in")
+
+let a (x: string) = x.Length
+let b (x: string) = x.Split(" ").Length
+
+let part1 = input |> Seq.sumBy a
+let part2 = input |> Seq.sumBy b
