0c126b28b128541f18834ec97670a7f155008408
Johan Litsfeldt <2831146+johlits@users.noreply.github.co>
2023-12-09 06:22:43 +0100

    Add day 10 2023

---
+++ aoc2023/day10/Day10.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Threading.Tasks.Sources;
+
+public class Day10
+{
+    public static void Run()
+    {
+        using (StreamReader file = new StreamReader("day10/p.in"))
+        {
+            string? ln;
+            while ((ln = file.ReadLine()) != null)
+            {
+                Console.WriteLine(ln);
+            }
+
+            file.Close();
+        }
+    }
+}
+
