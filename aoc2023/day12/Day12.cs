94e370c16ecf92923302103787814037891bdcb5
Johan Litsfeldt <2831146+johlits@users.noreply.github.co>
2023-12-11 08:30:08 +0100

    Add day 11 2023

---
+++ aoc2023/day12/Day12.cs
@@ -0,0 +1,18 @@
+﻿
+public class Day12
+{
+    public static void Run()
+    {
+        using (StreamReader file = new StreamReader("day12/p.in"))
+        {
+            string? ln;
+            while ((ln = file.ReadLine()) != null)
+            {
+                Console.WriteLine(ln);
+            }
+
+            file.Close();
+        }
+    }
+}
+
