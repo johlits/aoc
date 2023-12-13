b4351a8801726f6806aaf858dac200f0a1a6d9ef
Johan Litsfeldt <2831146+johlits@users.noreply.github.co>
2023-12-13 08:20:13 +0100

    2023 Day 13

--- aoc2023/day13/Day13.cs
+++ aoc2023/day13/Day13.cs
@@ -3,6 +3,25 @@ using System.Runtime.CompilerServices;
 
 public class Day13
 {
+    public static bool AreStringsOffByOne(string str1, string str2)
+    {
+        int diffCount = 0;
+
+        for (int i = 0; i < str1.Length; i++)
+        {
+            if (str1[i] != str2[i])
+            {
+                diffCount++;
+                if (diffCount > 1)
+                {
+                    return false;  
+                }
+            }
+        }
+
+        return diffCount == 1;  
+    }
+
     public static void Run()
     {
         using (StreamReader file = new StreamReader("day13/p.in"))
@@ -29,48 +48,46 @@ public class Day13
             {
                 // row by row
                 var rscore = 0;
-                //var rscore_cnt = 0;
                 var foundr = false;
 
                 for (var i = 1; i < m.Count; i++)
                 {
                     
-                    if (m[i] == m[i-1])
+                    if (m[i] == m[i-1] || AreStringsOffByOne(m[i], m[i-1]))
                     {
                         var rscore_temp = i;
                         var u = i - 1;
                         var d = i;
-                        //var score = 0;
-
-                        //score++;
-                        //if (score > rscore_cnt)
-                        //{
-                            rscore = rscore_temp;
-                            //rscore_cnt = score;
-                        //}
+                        rscore = rscore_temp;
 
                         var eq = true;
+                        var offs = AreStringsOffByOne(m[i], m[i - 1]) ? 1 : 0;
                         while (u > 0 && d < m.Count - 1)
                         {
-                            
 
                             u--;
                             d++;
 
                             if (m[u] != m[d])
                             {
-                                eq = false;
-                                break;
+                                if (AreStringsOffByOne(m[u], m[d]))
+                                {
+                                    offs++;
+                                }
+                                else
+                                {
+                                    eq = false;
+                                    break;
+                                }
+                                if (offs > 1)
+                                {
+                                    eq = false;
+                                    break;
+                                }
+                                
                             }
-
-                            //score++;
-                            //if (score > rscore_cnt)
-                            //{
-                                //rscore = rscore_temp;
-                                //rscore_cnt = score;
-                            //}
                         }
-                        if (eq)
+                        if (eq && offs == 1)
                         {
                             foundr = true;
                             break;
@@ -80,19 +97,23 @@ public class Day13
 
                 // col by col
                 var cscore = 0;
-                //var cscore_cnt = 0;
                 var foundc = false;
 
                 var len = m[0].Length;
                 for (var i = 1; i < len; i++)
                 {
                     var eq = true;
+                    var o = 0;
                     for (var j = 0; j < m.Count; j++)
                     {
                         if (m[j][i] != m[j][i - 1])
                         {
-                            eq = false;
-                            break;
+                            o++;
+                            if (o > 1)
+                            {
+                                eq = false;
+                                break;
+                            }
                         }
                     }
 
@@ -101,15 +122,9 @@ public class Day13
                         var cscore_temp = i;
                         var l = i - 1;
                         var r = i;
-                        //var score = 0;
+                        cscore = cscore_temp;
 
-                        //score++;
-                        //if (score > cscore_cnt)
-                        //{
-                            cscore = cscore_temp;
-                            //cscore_cnt = score;
-                        //}
-
+                        var offs = o;
                         while (l > 0 && r < len - 1)
                         {
 
@@ -121,24 +136,21 @@ public class Day13
                             {
                                 if (m[j][r] != m[j][l])
                                 {
-                                    eq = false;
-                                    break;
+                                    offs++;
+                                    if (offs > 1)
+                                    {
+                                        eq = false;
+                                        break;
+                                    }
                                 }
                             }
                             if (eq == false)
                             {
                                 break;
                             }
-
-                            //score++;
-                            //if (score > cscore_cnt)
-                            //{
-                            //    cscore = cscore_temp;
-                            //    cscore_cnt = score;
-                            //}
                         }
 
-                        if (eq)
+                        if (eq && offs == 1)
                         {
                             foundc = true;
                             break;
@@ -147,12 +159,10 @@ public class Day13
                 }
                 if (foundr)
                 {
-                    //Console.WriteLine(rscore * 100);
                     cnt += rscore * 100;
                 }
                 if (foundc)
                 {
-                    //Console.WriteLine(cscore);
                     cnt += cscore;
                 }
                 if (!foundr && !foundc)
@@ -163,7 +173,6 @@ public class Day13
                 {
                     throw new Exception();
                 }
-                //Console.WriteLine();
             }
 
             Console.WriteLine(cnt);
