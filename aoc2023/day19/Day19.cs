ad54e56524ddd4c85557c03a5abf1d505f949038
Johan Litsfeldt <2831146+johlits@users.noreply.github.co>
2023-12-18 11:28:50 +0100

    d18 y2023

---
+++ aoc2023/day19/Day19.cs
@@ -0,0 +1,17 @@
+﻿public class Day19
+{
+    public static void Run()
+    {
+        using (StreamReader file = new StreamReader("day19/p.in"))
+        {
+            string? ln;
+            while ((ln = file.ReadLine()) != null)
+            {
+                Console.WriteLine(ln);
+            }
+
+            file.Close();
+        }
+    }
+}
+
