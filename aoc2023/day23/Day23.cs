fd285ffbec17b0e752635558c0e437b4e37e0b5d
Johan Litsfeldt <johan.litsfeldt@uu.se>
2023-12-22 11:58:05 +0100

    d22 y2023

---
+++ aoc2023/day23/Day23.cs
@@ -0,0 +1,17 @@
+﻿public class Day23
+{
+    public static void Run()
+    {
+        using (StreamReader file = new StreamReader("day23/p.in"))
+        {
+            string? ln;
+            while ((ln = file.ReadLine()) != null)
+            {
+                Console.WriteLine(ln);
+            }
+
+            file.Close();
+        }
+    }
+}
+
