86225dfc20f6030c355c63de1f4ad0edbe531317
johlits <2831146+johlits@users.noreply.github.com>
2023-12-23 23:36:30 +0100

    d23 y2023

---
+++ aoc2023/day24/Day24.cs
@@ -0,0 +1,17 @@
+﻿public class Day24
+{
+    public static void Run()
+    {
+        using (StreamReader file = new StreamReader("day24/p.in"))
+        {
+            string? ln;
+            while ((ln = file.ReadLine()) != null)
+            {
+                Console.WriteLine(ln);
+            }
+
+            file.Close();
+        }
+    }
+}
+
