d54ea3df6aa2171f75281d6ed678b379fc9c8ab4
johlits <2831146+johlits@users.noreply.github.com>
2023-12-28 21:42:10 +0100

    d11 y2021

---
+++ aoc2021/day12/Day12.cs
@@ -0,0 +1,18 @@
+﻿using Helper;
+
+public class Day12
+{
+    public static void Run()
+    {
+        var numbers = new ListOfIntegers();
+        var bps = new List<(Blueprint, int)>
+        {
+            (numbers, -1),
+        };
+        new Parser("p.in", bps, new Symbols()
+        {
+
+        });
+    }
+}
+
