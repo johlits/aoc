b9bd8e69b6956c5407730724656fa21bc0e6aa23
johlits <2831146+johlits@users.noreply.github.com>
2023-12-28 15:38:22 +0100

    d6 y2021

--- aoc2021/day06/Day6.cs
+++ aoc2021/day06/Day6.cs
@@ -1,4 +1,5 @@
 ﻿using Helper;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 public class Day6
 {
@@ -13,6 +14,23 @@ public class Day6
         {
 
         });
+
+        long[] fish = new long[9];
+        foreach (var f in numbers.lists.First().list)
+        {
+            fish[f]++;
+        }
+
+        for (int day = 0; day < 256; day++)
+        {
+            long newFish = fish[0];
+            Array.Copy(fish, 1, fish, 0, fish.Length - 1);
+            fish[6] += newFish; 
+            fish[8] = newFish; 
+        }
+
+        long totalFish = fish.Sum();
+        Console.WriteLine(totalFish);
     }
 }
 
---
+++ aoc2021/day07/Day7.cs
@@ -0,0 +1,18 @@
+﻿using Helper;
+
+public class Day7
+{
+    public static void Run()
+    {
+        var numbers = new ListOfIntegers();
+        var bps = new List<(Blueprint, int)>
+        {
+            (numbers, -1),
+        };
+        new Parser("day07/p.in", bps, new Symbols()
+        {
+
+        });
+    }
+}
+
